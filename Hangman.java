09c2f3b89b7051b657c9c72931f46098ce2cf0eb
valeriequintana <qvalerie@live.com>
2015-12-04 13:30:38 -0500

    Update Hangman.java

--- Hangman.java
+++ Hangman.java
@@ -1,17 +1,25 @@
 /*
  * Project 1
  * Hangman.java
+ * Team: Valerie Quintana, Aditi Pathak, Cheryl Litwinczuk, 
+ *       Claudio Sousa, Don Hwan Kim, Yan Ling
  */
 
 package hangman;
+// Input scanner to take in all the text the user types
 import java.util.Scanner;
+
+// This is the main class where all events will be processed
 public class Hangman
 {
   public static void main(String[] args)
   {
+// This is where our game will start
     Scanner guess = new Scanner(System.in);
+// This displays "Hello, and welcome to Hangman!" on screen.
     System.out.println("Hello, and welcome to Hangman!");
     System.out.println("Press 1 to Play!");
+// If user presses 1, it will create a Play object called obj and tell it to start 
     int yes = guess.nextInt(); 
     if(yes==1)
     {
